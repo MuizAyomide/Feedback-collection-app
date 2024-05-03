70f4563474712f1480f40263c3bb1cd283be7ff7
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-03 10:39:37 +0100

    App update

--- Feedback-System/src/App.jsx
+++ Feedback-System/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './../src/App.css'
 import Navbar from './Components/Navbar/Navbar'
 import big_logo from './assets/big-logo.png'
+import Home from './Components/Home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
-
       <>
       <img className='bg-img' src={big_logo} alt="" />
      </>
-
+<Home/>
     </div>
   )
 }
