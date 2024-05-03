647b1613fb86345398ad68b40d1091b3af405b46
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-03 10:39:54 +0100

    home update

---
+++ Feedback-System/src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Welcome to a FeedBack Collection Application</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+        nesciunt, ipsum, fugit nulla laboriosam alias perspiciatis et possimus
+        laborum voluptate incidunt! Autem rerum, debitis beatae nulla quaerat
+        cumque! Nemo, maiores!
+      </p>
+
+      <button>Login</button>
+      <button>Register</button>
+
+    </div>
+  );
+};
+
+export default Home;
