c61973db9a8ac2cb894e3e4a0356f4730bff205f
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-05 21:47:54 +0100

    app update

--- Feedback-System/src/App.jsx
+++ Feedback-System/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import big_logo from './assets/big-logo.png'
 import Home from './Components/Home/Home'
 import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
      </>
 {/* <Home/> */}
 <Login/>
+{/* <Register/> */}
     </div>
   )
 }
