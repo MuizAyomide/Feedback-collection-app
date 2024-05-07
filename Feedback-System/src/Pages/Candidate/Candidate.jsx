ecb8e4aafae906e094c337fbae3d180e4456965e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-07 15:53:35 +0100

    candidate update

--- Feedback-System/src/Pages/Candidate/Candidate.jsx
+++ Feedback-System/src/Pages/Candidate/Candidate.jsx
@@ -1,11 +1,126 @@
-import React from 'react'
-import './Candidate.css'
+import React from "react";
+import "./Candidate.css";
 const Candidate = () => {
   return (
-    <div className='candidate'>
-      
-    </div>
-  )
-}
+    <div className="candidate">
+      <div className="candidate-text">
+        <h3>
+          Welcome <span>MUIZ</span>!
+        </h3>
+        <h1>CANDIDATE FEEDBACK</h1>
+        <h3>Enter your feedback...</h3>
+        <form>
+          <input type="text" />
+          <button>SEND</button>
+        </form>
+      </div>
 
-export default Candidate
+      <div className="other-feedback">
+        <h3>Other Feedbacks</h3>
+        <div className="feedbacks">
+
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="candidatename-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Candidate;
