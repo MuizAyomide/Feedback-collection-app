095b71761ca302eb615ad2cdacf43d068d6a908b
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-07 15:54:04 +0100

    user update

--- Feedback-System/src/Pages/User/User.jsx
+++ Feedback-System/src/Pages/User/User.jsx
@@ -1,11 +1,126 @@
-import React from 'react'
-import './User.css'
+import React from "react";
+import "./User.css";
 const User = () => {
   return (
-    <div className='user'>
-      
-    </div>
-  )
-}
+    <div className="user">
+      <div className="user-text">
+        <h3>
+          Welcome <span>MUIZ</span>!
+        </h3>
+        <h1>USER FEEDBACK</h1>
+        <h3>Enter your feedback...</h3>
+        <form>
+          <input type="text" />
+          <button>SEND</button>
+        </form>
+      </div>
 
-export default User
+      <div className="other-feedback">
+        <h3>Other Feedbacks</h3>
+        <div className="feedbacks">
+
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="username-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
