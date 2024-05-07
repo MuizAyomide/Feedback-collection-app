042b6c2436318c087193299ebfcd4c566e34a47d
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-07 15:53:49 +0100

    product update

--- Feedback-System/src/Pages/Product/Product.jsx
+++ Feedback-System/src/Pages/Product/Product.jsx
@@ -1,11 +1,126 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
 const Product = () => {
   return (
-    <div className='product'>
-      
-    </div>
-  )
-}
+    <div className="product">
+      <div className="product-text">
+        <h3>
+          Welcome <span>MUIZ</span>!
+        </h3>
+        <h1>PRODUCT FEEDBACK</h1>
+        <h3>Enter your feedback...</h3>
+        <form>
+          <input type="text" />
+          <button>SEND</button>
+        </form>
+      </div>
 
-export default Product
+      <div className="other-feedback">
+        <h3>Other Feedbacks</h3>
+        <div className="feedbacks">
+
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        <div className="feedback">
+          <div className="productname-text">M</div>
+          <div className="feedback-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            possimus quam saepe quas repellendus!
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
