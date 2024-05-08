72c2597809082eb65980b4f372491d1e35620146
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 15:11:37 +0100

    category update

--- Feedback-System/src/Pages/Category/Category.jsx
+++ Feedback-System/src/Pages/Category/Category.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Category.css'
+import { Link } from 'react-router-dom'
 const Category = () => {
 
 
@@ -7,18 +8,23 @@ const Category = () => {
     <div className='category'>
       <h3>Select Category...</h3>
       <div className="cards">
+      <Link to={'/user'} style={{textDecoration: 'none', color: 'black'}}> 
         <div className="card">
             <h2>USER</h2>
             <p>Feedback</p>
         </div>
+      </Link> 
+      <Link to={'/product'} style={{textDecoration: 'none', color: 'black'}}> 
         <div className="card">
             <h2>Product</h2>
             <p>Feedback</p>
         </div>
+      </Link><Link to={'/candidate'} style={{textDecoration: 'none', color: 'black'}}> 
         <div className="card">
             <h2>Candidate</h2>
             <p>Feedback</p>
         </div>
+      </Link>
       </div>
     </div>
   )
