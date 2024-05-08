cb5440902fd9634f17cbbedfbd82777009f79c3a
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 15:11:11 +0100

    register update

--- Feedback-System/src/Components/Register/Register.jsx
+++ Feedback-System/src/Components/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Register.css";
+import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <div className="register">
@@ -7,11 +8,19 @@ const Register = () => {
         <input type="text" placeholder="Username:" />
         <input type="text" placeholder="Email:" />
         <input type="text" placeholder="Password:" />
-        <div className='mid-info'>
-        <h3>Forgot Password?</h3>
-        <h3>Already have an account? <span>Login Here!</span></h3>
-      </div>
-        <button>Register</button>
+        <div className="mid-info">
+         
+          <h3> <input type="checkbox" className="checkbox"/> Terms and Services</h3>
+          
+          <h3>
+            Already have an account?{" "}
+            <Link to={"/login"} style={{ textDecoration: "none" }}>
+              {" "}
+              <span>Login Here!</span>
+            </Link>
+          </h3>
+        </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
