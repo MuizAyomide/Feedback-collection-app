11915c80ff014cb73bf58c50730f5c26383885d9
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 15:10:34 +0100

    home update

--- Feedback-System/src/Components/Home/Home.jsx
+++ Feedback-System/src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.css";
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
   return (
@@ -12,8 +14,8 @@ const Home = () => {
         cumque! Nemo, maiores!
       </p>
 <div className="btn">
-      <button className="login-btn">Login</button>
-      <button className="register-btn">Register</button>
+  <Link to='/login' style={{textDecoration: 'none'}}>   <button className="login-btn">Login</button></Link> 
+  <Link to='/register' style={{textDecoration: 'none'}}>  <button className="register-btn">Register</button></Link> 
 </div>
 
     </div>
