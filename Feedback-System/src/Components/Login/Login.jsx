348f77a458ddd96f5f2bb4277f662b37754b60ff
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-10 18:42:56 +0100

    login update

--- Feedback-System/src/Components/Login/Login.jsx
+++ Feedback-System/src/Components/Login/Login.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import './Login.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-
-  const handleLogin = e =>{
-    e.preventDefault()
-  }
-  
-
-
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div className='login'>
+    <div className="login">
       <form onSubmit={handleLogin}>
-        <input type="text" placeholder='Email:' name='email' />
-        <input type="text" placeholder='Password:' name='password' />
-        <Link to={'/category'} style={{textDecoration: 'none'}}> <button>Login</button></Link>
+        <input type="text" placeholder="Email:" name="email" />
+        <input type="text" placeholder="Password:" name="password" />
+        <Link to={"/category"} style={{ textDecoration: "none" }}>
+          {" "}
+          <button>Login</button>
+        </Link>
       </form>
-      <div className='bottom-info'>
+      <div className="bottom-info">
         <h3>Forgot Password?</h3>
-        <h3>Don't have an account?<Link to={'/register'} style={{textDecoration: 'none'}}> <span>Register Here!</span></Link></h3>
+        <h3>
+          Don't have an account?
+          <Link to={"/register"} style={{ textDecoration: "none" }}>
+            {" "}
+            <span>Register Here!</span>
+          </Link>
+        </h3>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
