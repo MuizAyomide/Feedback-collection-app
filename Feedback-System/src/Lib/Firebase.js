62e2f2d067eda89ec61b1eca1faed064d66fed3c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-10 18:43:24 +0100

    firebase update

--- Feedback-System/src/Lib/Firebase.js
+++ Feedback-System/src/Lib/Firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/fireStore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -15,3 +15,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
+
