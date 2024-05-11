d19981b8867b288f622fcba2443803f234aaceeb
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-11 20:01:11 +0100

    app update

--- Feedback-System/src/App.jsx
+++ Feedback-System/src/App.jsx
@@ -14,19 +14,24 @@ import Contact from "./Components/Contact/Contact";
 import Notification from "./Components/Notification/Notifcation";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Lib/Firebase";
+import { useUserStore } from "./Lib/userStore";
 
 const App = () => {
+  const { currentUser, fetchUserInfo } = useUserStore();
+
   const user = true;
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      fetchUserInfo(user.uid);
     });
 
     return () => {
       unSub();
     };
-  }, []);
+  }, [fetchUserInfo]);
+
+  console.log(currentUser.id);
 
   return (
     <div className="app">
@@ -36,14 +41,21 @@ const App = () => {
           <img className="bg-img" src={big_logo} alt="" />
         </>
         <Routes>
+          {
+            currentUser ? <>
+            <Route path="/category" element={<Category />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/candidate" element={<Candidate />} />
+            
+            </>:<>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/candidate" element={<Candidate />} />
+            
+            </>
+          }
         </Routes>
       </BrowserRouter>
       <Notification />
