40f3e630487dc78ba1b65e5a5211732ce88e7bc7
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-14 02:10:07 +0100

    userStore update

--- Feedback-System/src/Lib/userStore.js
+++ Feedback-System/src/Lib/userStore.js
@@ -1,28 +1,26 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { toast } from 'react-toastify';
-import { create } from 'zustand'
-import { db } from './Firebase';
+import { doc, getDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { create } from "zustand";
+import { db } from "./Firebase";
 
 export const useUserStore = create((set) => ({
+  currentUser: "",
+  fetchUserInfo: async (uid) => {
+    if (!uid) return set({ currentUser: null });
 
-  currentUser: '',
-  fetchUserInfo: async (uid) =>{
-    if(!uid) return set({currentUser:null});
+    try {
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
 
-    try{
-        const docRef = doc(db, "users", uid);
-const docSnap = await getDoc(docRef);
-
-if (docSnap.exists()) {
-  set({currentUser: docSnap.data()})
-} else {
-    set({currentUser: null})
-}
-
-    }catch(err){
-        // toast(err.message)
-        console.log(err)
-        return set({currentUser:null});
+      if (docSnap.exists()) {
+        set({ currentUser: docSnap.data() });
+      } else {
+        set({ currentUser: null });
+      }
+    } catch (err) {
+      // toast(err.message)
+      console.log(err);
+      return set({ currentUser: null });
     }
-  }
-}))
+  },
+}));
