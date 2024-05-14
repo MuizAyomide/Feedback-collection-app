40fbc873cc8fa1891e53c02736573d3e20079f64
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-14 02:10:17 +0100

    user update

--- Feedback-System/src/Pages/User/User.jsx
+++ Feedback-System/src/Pages/User/User.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./User.css";
 import { useUserStore } from "../../Lib/userStore";
-import { doc, onSnapshot } from "firebase/firestore";
+import { arrayUnion, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../Lib/Firebase";
 
 const User = () => {
-  
   const [text, setText] = useState("");
 
   const [feedback, setFeedback] = useState();
   const { currentUser } = useUserStore();
 
   useEffect(() => {
-    const unSub = onSnapshot(doc(db, "userfeedback", currentUser.id), (doc) => {
+    const unSub = onSnapshot(doc(db, "users", currentUser.id), (doc) => {
       setFeedback(doc.data());
     });
 
@@ -20,7 +19,23 @@ const User = () => {
       unSub();
     };
   }, [currentUser.id]);
+  console.log(currentUser.id);
 
+  const handleSend = async () => {
+    if (text === "") return;
+
+    try {
+      await updateDoc(db, "userfeedback", feedback.id, {
+        feedbacks: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="user">
@@ -30,23 +45,28 @@ const User = () => {
         </h3>
         <h1>USER FEEDBACK</h1>
         <h3>Enter your feedback...</h3>
-        <form>
-          <input type="text" 
-          value={text}
-          onChange={(e) => setText(e.target.value)}/>
-          <button>SEND</button>
-        </form>
+        <div className="form">
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={handleSend}>SEND</button>
+        </div>
       </div>
 
       <div className="other-feedback">
         <h3>Other Feedbacks</h3>
         <div className="feedbacks">
           <div className="feedback">
-            <div className="username-text">M</div>
-            <div className="feedback-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              possimus quam saepe quas repellendus!
+            <div className="username-text">
+              {currentUser.username.slice(0, 1).toUpperCase()}
             </div>
+            {feedback?.feedbacks?.map((feeds) => (
+              <div className="feedback-text" key={feeds?.createdAt}>
+                {feeds.text}
+              </div>
+            ))}
           </div>
         </div>
       </div>
