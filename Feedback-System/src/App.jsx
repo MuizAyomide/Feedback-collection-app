331c51113c2a497fb0a6c2c994b73bb8a0f624b8
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-15 10:22:59 +0100

    app update

--- Feedback-System/src/App.jsx
+++ Feedback-System/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import big_logo from "./assets/big-logo.png";
@@ -10,10 +11,10 @@ import User from "./Pages/User/User";
 import Product from "./Pages/Product/Product";
 import Candidate from "./Pages/Candidate/Candidate";
 import Contact from "./Components/Contact/Contact";
-import Notification from "./Components/Notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Lib/Firebase";
 import { useUserStore } from "./Lib/userStore";
+import Notification from "./Components/Notification/Notifcation";
 
 const App = () => {
   const { currentUser, fetchUserInfo } = useUserStore();
@@ -34,7 +35,9 @@ const App = () => {
     <div className="app">
       <BrowserRouter>
         <Navbar />
+        <>
         <img className="bg-img" src={big_logo} alt="" />
+        </>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -50,7 +53,7 @@ const App = () => {
           )}
         </Routes>
       </BrowserRouter>
-      <Notification />
+      <Notification/>
     </div>
   );
 };
