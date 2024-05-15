041edc2661387d7dcc124c33e3d3affde6805cbf
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-15 23:57:39 +0100

    commit

--- Feedback-System/src/Components/Notification/Notifcation.jsx
+++ Feedback-System/src/Components/Notification/Notifcation.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Notification.css'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import "./Notification.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Notification = () => {
   return (
     <div>
-      <ToastContainer position='bottom-right'/>
+      <ToastContainer position="bottom-right" />
     </div>
-  )
-}
+  );
+};
 
 export default Notification;
