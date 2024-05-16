9563c34b29145272b3e5f5c95f489d35e57eb14b
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-16 15:45:03 +0100

    commit

--- Feedback-System/src/AdminPage/AdminUserFeedback/AdminUserFeedback.jsx
+++ Feedback-System/src/AdminPage/AdminUserFeedback/AdminUserFeedback.jsx
@@ -3,8 +3,49 @@ import './AdminUserFeedback.css'
 
 const AdminUserFeedback = () => {
   return (
-    <div>
-      
+    <div className="adminuser">
+      <div className="adminuser-text">
+        <h1>USER FEEDBACKS</h1>
+      </div>
+
+      <div className="other-feedback">
+            <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+      </div>
     </div>
   )
 }
