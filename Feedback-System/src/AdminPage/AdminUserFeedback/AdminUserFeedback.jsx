d3aa02d405b559a68c5e592b8c62e987aa26ecbd
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-16 21:47:36 +0100

    commit

--- Feedback-System/src/AdminPage/AdminUserFeedback/AdminUserFeedback.jsx
+++ Feedback-System/src/AdminPage/AdminUserFeedback/AdminUserFeedback.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-import './AdminUserFeedback.css'
+import React, { useEffect, useState } from 'react';
+
 
 const AdminUserFeedback = () => {
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  useEffect(() => {
+    // Fetch the feedbacks from Firestore
+    const fetchFeedbacks = async () => {
+      try {
+        const feedbacksRef = firebase.firestore().collection('UserFeedbacks');
+        const snapshot = await feedbacksRef.get();
+        const fetchedFeedbacks = snapshot.docs.map(doc => doc.data());
+        setFeedbacks(fetchedFeedbacks);
+      } catch (error) {
+        console.error('Error fetching feedbacks:', error);
+      }
+    };
+
+    // Call the fetchFeedbacks function
+    fetchFeedbacks();
+  }, []);
+
   return (
     <div className="adminuser">
       <div className="adminuser-text">
@@ -9,45 +28,15 @@ const AdminUserFeedback = () => {
       </div>
 
       <div className="other-feedback">
-            <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
-        <div className="feedback">
-              <div className="candidatename-text">M</div>
-              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
-        </div>
+        {feedbacks.map((feedback, index) => (
+          <div className="feedback" key={index}>
+            <div className="candidatename-text">{feedback.candidateName}</div>
+            <div className="feedback-text">{feedback.message}</div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminUserFeedback
+export default AdminUserFeedback;
