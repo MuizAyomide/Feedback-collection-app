e283978e7e3ca98879f592b642e43d4c9ca9c552
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-16 01:29:14 +0100

    admin category created

---
+++ Feedback-System/src/AdminPage/AdminCategory/AdminCategory.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './AdminCategory.css'
+import { Link } from 'react-router-dom'
+
+const AdminCategory = () => {
+  return (
+    <div>
+        <div className="category">
+      <h3>Select Category...</h3>
+      <div className="cards">
+        <Link to={"/user"} style={{ textDecoration: "none", color: "black" }}>
+          <div className="card">
+            <h2>USER</h2>
+            <p>Feedback</p>
+          </div>
+        </Link>
+        <Link
+          to={"/product"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <div className="card">
+            <h2>Product</h2>
+            <p>Feedback</p>
+          </div>
+        </Link>
+        <Link
+          to={"/candidate"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <div className="card">
+            <h2>Candidate</h2>
+            <p>Feedback</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+      
+    </div>
+  )
+}
+
+export default AdminCategory
