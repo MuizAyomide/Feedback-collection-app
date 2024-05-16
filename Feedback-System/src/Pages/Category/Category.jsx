559ab6c5150a7661e33183fc7904bf7bb861ea80
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-16 21:47:21 +0100

    category update

--- Feedback-System/src/Pages/Category/Category.jsx
+++ Feedback-System/src/Pages/Category/Category.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Category.css";
 import { Link } from "react-router-dom";
 const Category = () => {
+  
   return (
     <div className="category">
       <h3>Select Category...</h3>
