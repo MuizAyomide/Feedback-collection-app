39d7fc324a4bbc9b6864f038f368fd30472f6519
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-16 15:45:28 +0100

    adminproductfeedback creation

---
+++ Feedback-System/src/AdminPage/AdminProductFeedback/AdminProductFeedback.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './AdminProductFeedback.css'
+
+const AdminProductFeedback = () => {
+  return (
+    <div className="adminproduct">
+    <div className="adminproduct-text">
+        <h1>PRODUCT FEEDBACKS</h1>
+      </div>
+
+      <div className="other-feedback">
+            <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+        <div className="feedback">
+              <div className="candidatename-text">M</div>
+              <div className="feedback-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla!</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AdminProductFeedback
