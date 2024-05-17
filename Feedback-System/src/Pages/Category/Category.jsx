00207ad2f6c674c95c261602a7c909bd8a521f7d
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-17 23:56:48 +0100

    commit

--- Feedback-System/src/Pages/Category/Category.jsx
+++ Feedback-System/src/Pages/Category/Category.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Category.css";
 import { Link } from "react-router-dom";
 const Category = () => {
-  
   return (
     <div className="category">
       <h3>Select Category...</h3>
